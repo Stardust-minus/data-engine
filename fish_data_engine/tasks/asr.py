10374bd7ee8f1b0272df931d7475a6850706dfd3
Lengyue <lengyue@lengyue.me>
2023-12-10 05:33:59 -0500

    Optimize pipeline

--- fish_data_engine/tasks/asr.py
+++ fish_data_engine/tasks/asr.py
@@ -16,13 +16,15 @@ import sys
 
 
 class ASR(Task):
-    def __init__(self, input_dir, output_dir, demucs, vad, language, punctuation):
+    def __init__(
+        self, input_dir, output_dir, demucs, speaker_separation, language, punctuation
+    ):
         super().__init__()
 
         self.input_dir = input_dir
         self.output_dir = output_dir
         self.demucs_model = None
-        self.vad_pipeline = None
+        self.speaker_separation_pipeline = None
 
         if not IS_WORKER:
             return
@@ -32,17 +34,17 @@ class ASR(Task):
             logger.debug("Loading demucs model")
             self.demucs_model = get_demucs_model("htdemucs")
             self.demucs_model.eval()
-            self.demucs_model.to(torch.device("cuda"))
+            self.demucs_model.cuda()
             logger.debug("Demucs model loaded")
 
         # Voice activity detection
-        if vad:
-            logger.debug("Loading VAD model")
-            self.vad_pipeline = PyannotePipeline.from_pretrained(
+        if speaker_separation:
+            logger.debug("Loading speaker separation model")
+            self.speaker_separation_pipeline = PyannotePipeline.from_pretrained(
                 "pyannote/speaker-diarization-3.1",
             )
-            self.vad_pipeline.to(torch.device("cuda"))
-            logger.debug("VAD model loaded")
+            self.speaker_separation_pipeline.to(torch.device("cuda"))
+            logger.debug("Speaker separation model loaded")
 
         # ASR
         assert language in [
@@ -51,13 +53,12 @@ class ASR(Task):
         ], "Language must be either zh or en, whisper is not supported yet"
 
         MODELS = {
-            "zh": "damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
+            "zh": "damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
             "en": "damo/speech_paraformer-large-vad-punc_asr_nat-en-16k-common-vocab10020",
         }
 
         kwargs = {
             "model": MODELS[language],
-            "vad_model": "",  # Use our own VAD model
         }
 
         if punctuation is False:
@@ -77,15 +78,18 @@ class ASR(Task):
 
     @torch.no_grad()
     def process(self, job):
+        # Always clear GPU cache before processing a new job
+        torch.cuda.empty_cache()
+
         audio, sr = torchaudio.load(job)
         logger.debug(f"Audio loaded, duration={audio.shape[1]/sr:.1f}s")
         audio = audio.cuda()
 
         relative_path = job.relative_to(self.input_dir)
-        output_path = self.output_dir / relative_path
-        output_path.parent.mkdir(parents=True, exist_ok=True)
+        output_path = self.output_dir / relative_path.parent / relative_path.stem
+        output_path.mkdir(parents=True, exist_ok=True)
 
-        if output_path.with_suffix(".asr.json").exists():
+        if (output_path / "asr.json").exists():
             logger.debug(f"ASR already done for {relative_path}")
             return
 
@@ -95,48 +99,79 @@ class ASR(Task):
 
         # Extract vocals
         if self.demucs_model:
-            # Apply demucs model
             audio, sr = self.apply_demucs(audio, sr)
+            torch.cuda.empty_cache()
 
-            # Sample to 16kHz and save
-            audio, sr = (
-                torchaudio.functional.resample(audio, orig_freq=sr, new_freq=16000),
-                16000,
-            )
-            sf.write(output_path.with_suffix(".vocals.mp3"), audio[0].cpu().numpy(), sr)
+        # Sample to 16kHz
+        audio, sr = (
+            torchaudio.functional.resample(audio, orig_freq=sr, new_freq=16000),
+            16000,
+        )
 
-        # VAD
-        if not self.vad_pipeline:
-            segments = {
-                "SPEAKER_0": [(0, audio.shape[-1] / sr)],
-            }
+        # Speaker separation
+        if not self.speaker_separation_pipeline:
+            segments = [[(0, audio.shape[-1] / sr)]]
         else:
-            segments = self.apply_vad(audio, sr)
+            segments = self.apply_speaker_separation(audio, sr)
 
-            with open(output_path.with_suffix(".vad.json"), "w") as f:
+            with open(output_path / "vad.json", "w") as f:
                 json.dump(segments, f)
 
         if len(segments) == 0:
             logger.debug("No speaker found")
             return
 
-        # ASR
-        asr_results = {}
-        logger.debug("Start ASR")
-
-        for speaker, turns in segments.items():
-            texts = []
+        # Build speaker segments mask on CPU
+        speaker_segments_masks = torch.zeros(
+            (len(segments), audio.shape[-1]), dtype=torch.int
+        )
+        for i, turns in enumerate(segments):
             for start, end in turns:
-                audio = audio[:, int(start * sr) : int(end * sr)]
-                text = self.asr_pipeline(audio_in=audio, sample_rate=sr)["text"]
-                texts.append({"start": start, "end": end, "text": text})
+                if start >= end:
+                    continue
 
-            asr_results[speaker] = texts
+                speaker_segments_masks[i, int(start * sr) : int(end * sr)] = 1
+
+        # Calculate overlapping and remove overlapping segments
+        summed_speaker_segments_masks = speaker_segments_masks.sum(0)
+        overlapping_segments_mask = summed_speaker_segments_masks > 1
+        speaker_segments_masks[:, overlapping_segments_mask] = 0
+        logger.debug(f"Removed {overlapping_segments_mask.sum()} overlapping segments")
+
+        # ASR
+        logger.debug("Start ASR")
+        asr_results = []
+
+        for spk_id, mask in enumerate(speaker_segments_masks):
+            if mask.sum() == 0:
+                continue
+
+            coped_audio = audio.clone()
+            coped_audio[:, mask == 0] = 0
+
+            # Save the audio
+            sf.write(
+                output_path / f"speaker-{spk_id}.mp3", coped_audio[0].cpu().numpy(), sr
+            )
+
+            # ASR
+            sentences = self.asr_pipeline(
+                audio_in=coped_audio[0].cpu().numpy(), sample_rate=sr
+            )["sentences"]
+            asr_result = [
+                {
+                    "start": s["start"] / sr,
+                    "end": s["end"] / sr,
+                    "text": s["text"],
+                }
+                for s in sentences
+            ]
+
+            asr_results.append(asr_result)
 
         logger.debug("ASR done")
-
-        with open(output_path.with_suffix(".asr.json"), "w") as f:
-            json.dump(asr_results, f)
+        with open(output_path / "asr.json", "w") as f:
+            json.dump(asr_results, f, ensure_ascii=False)
 
     def apply_demucs(self, audio, sr):
         logger.debug("Apply demucs model")
@@ -176,9 +211,11 @@ class ASR(Task):
 
         return audio, sr
 
-    def apply_vad(self, audio, sr):
-        logger.debug("Apply VAD model")
-        diarization = self.vad_pipeline({"waveform": audio, "sample_rate": sr})
+    def apply_speaker_separation(self, audio, sr):
+        logger.debug("Apply speaker separation model")
+        diarization = self.speaker_separation_pipeline(
+            {"waveform": audio, "sample_rate": sr}
+        )
 
         # Extract segments for each speaker
         speaker_to_turns = {}
@@ -202,7 +239,7 @@ class ASR(Task):
         logger.debug(f"VAD model applied, found {len(speaker_to_turns)} speakers")
 
         # Calculate the total length of the audio in seconds
-        total_audio_length = audio.shape[-1] / sr
+        audio.shape[-1] / sr
 
         # Initialize a new dictionary for filtered speaker turns
         filtered_speaker_to_turns = {}
@@ -211,10 +248,7 @@ class ASR(Task):
             total_speaking_time = sum(end - start for start, end in turns)
 
             # Check if total speaking time is more than 60s and more than 10% of the audio length
-            if (
-                total_speaking_time > 60
-                and total_speaking_time > 0.1 * total_audio_length
-            ):
+            if total_speaking_time > 60:
                 filtered_speaker_to_turns[speaker] = turns
 
         # Output the filtered speaker turns
@@ -222,61 +256,25 @@ class ASR(Task):
             f"After filtering, found {len(filtered_speaker_to_turns)} speakers"
         )
 
-        # Initialize a new dictionary for processed speaker turns
-        processed_speaker_to_turns = {}
-
-        for speaker, turns in filtered_speaker_to_turns.items():
-            if len(turns) == 1:
-                processed_speaker_to_turns[speaker] = turns
-                continue
-
-            merged_turns = []
-            current_start, current_end = turns[0]
-
-            for start, end in turns[1:]:
-                # Split the segment if its duration exceeds 30 seconds
-                if current_end - current_start > 30:
-                    merged_turns.append((current_start, current_end))
-                    current_start, current_end = start, end
-                    continue
-
-                # Check if the gap between segments is 1 second or less
-                if start - current_end <= 1:
-                    # Extend the current segment
-                    current_end = end
-                else:
-                    # Add the current segment and start a new one
-                    merged_turns.append((current_start, current_end))
-                    current_start, current_end = start, end
-
-            # Add the last segment
-            merged_turns.append((current_start, current_end))
-            processed_speaker_to_turns[speaker] = merged_turns
-
-        logger.debug(
-            f"VAD done, found {sum(len(turns) for turns in processed_speaker_to_turns.values())} segments"
-        )
-
-        return processed_speaker_to_turns
+        return list(filtered_speaker_to_turns.values())
 
 
 @click.command()
 @click.option("--input-dir", type=str, required=True)
-@click.option("--output-dir", type=str, required=False)
+@click.option("--output-dir", type=str, required=True)
 @click.option("--demucs/--no-demucs", default=True)
-@click.option("--vad/--no-vad", default=True)
+@click.option("--speaker-separation/--no-speaker-separation", default=True)
 @click.option("--punctuation/--no-punctuation", default=True)
 @click.option("--language", type=str, default="zh")
 @click.option("--debug/--no-debug", default=False)
-def main(input_dir, output_dir, demucs, vad, language, punctuation, debug):
-    if output_dir is None:
-        output_dir = input_dir
-
+def main(
+    input_dir, output_dir, demucs, speaker_separation, language, punctuation, debug
+):
     if debug:
         logger.remove()
         logger.add(sys.stderr, level="DEBUG")
 
-    task = ASR(input_dir, output_dir, demucs, vad, language, punctuation)
+    task = ASR(input_dir, output_dir, demucs, speaker_separation, language, punctuation)
     task.run()
 
 
